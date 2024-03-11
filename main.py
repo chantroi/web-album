cd0794a2104570be9cd4e69f4e25377742d27ab0
chantroi <tonglanhchua@gmail.com>
2024-03-11 22:35:33 +0700

    update main.py

--- main.py
+++ main.py
@@ -6,20 +6,12 @@ import os
 app = Quart(__name__)
 s3 = S3()
 
-def js():
-    scripts = list()
-    for jsf in os.listdir('static/js'):
-        if jsf != "script.js":
-            with open(f"static/js/{jsf}", 'r') as f:
-                scripts.append(f.read())
-    return "\n".join(scripts)
-                
-
 @app.route("/")
 async def home():
+    js_tag = "<script src='/static/js/form.js'></script>"
     return await render_template(
         "index.html",
-        all_js_script=js()
+        all_js_script=js_tag
         )
     
 @app.route("/s3/upload", methods=['POST'])
