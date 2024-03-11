128fd8b1f38248a2273a2d14e2c3730c1cf3277a
chantroi <tonglanhchua@gmail.com>
2024-03-11 18:13:11 +0700

    update res/base.py

--- res/base.py
+++ res/base.py
@@ -7,30 +7,30 @@ class S3Manager:
     def __init__(self):
         access_key = os.getenv('ACCESS_KEY')
         secret_key = os.getenv('SECRET_KEY')
-        self.bucket_name = "storage"
-        self.s3_client = boto3.client('s3', aws_access_key_id=access_key, aws_secret_access_key=secret_key)
+        self.bucket = "storage"
+        self.s3 = boto3.client('s3', aws_access_key_id=access_key, aws_secret_access_key=secret_key)
 
-    def upload_file(self, file_path, object_name=None):
+    def upload(self, file_obj, object_name=None):
         if object_name is None:
-            object_name = file_path
+            object_name = file_obj
         try:
-            self.s3_client.upload_file(file_path, self.bucket_name, object_name)
+            self.s3.upload_fileobj(file_obj, self.bucket, object_name)
             print("Upload successful")
         except FileNotFoundError:
             print("The file was not found")
         except NoCredentialsError:
             print("Credentials not available")
 
-    def download_file(self, object_name, file_path):
+    def download(self, object_name, file_obj):
         try:
-            self.s3_client.download_file(self.bucket_name, object_name, file_path)
+            self.s3.download_fileobj(self.bucket, object_name, file_obj)
             print("Download successful")
         except NoCredentialsError:
             print("Credentials not available")
 
     def list_files(self):
         try:
-            response = self.s3_client.list_objects_v2(Bucket=self.bucket_name)
+            response = self.s3.list_objects_v2(Bucket=self.bucket)
             if 'Contents' in response:
                 for obj in response['Contents']:
                     print(obj['Key'])
@@ -39,9 +39,9 @@ class S3Manager:
         except NoCredentialsError:
             print("Credentials not available")
 
-    def generate_public_link(self, object_name, expiration=3600):  # Expiration time in seconds
+    def get_link(self, object_name, expiration=360000):
         try:
-            url = self.s3_client.generate_presigned_url('get_object', Params={'Bucket': self.bucket_name, 'Key': object_name}, ExpiresIn=expiration)
+            url = self.s3.generate_presigned_url('get_object', Params={'Bucket': self.bucket, 'Key': object_name}, ExpiresIn=expiration)
             print("Public link:", url)
         except NoCredentialsError:
             print("Credentials not available")
